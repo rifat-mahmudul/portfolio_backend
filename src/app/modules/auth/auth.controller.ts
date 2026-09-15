82d310525510c7e88ba9583dc4c82a0ed9b7a8ec
rifat-mahmudul <rifatmahmudul.dev@gmail.com>
2026-09-15 13:31:26 +0600

    feat: implement authentication module with login functionality and route setup

---
+++ src/app/modules/auth/auth.controller.ts
@@ -0,0 +1,22 @@
+import { Request, Response } from "express";
+import { catchAsync } from "../../utils/catchAsync";
+import { AuthServices } from "./auth.service";
+import { sendResponse } from "../../utils/sendResponse";
+import httpStatusCode from "http-status-codes";
+
+const login = catchAsync(async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+
+  const result = await AuthServices.login({ email, password });
+
+  sendResponse(res, {
+    success: true,
+    statusCode: httpStatusCode.OK,
+    message: "Login successful.",
+    data: result,
+  });
+});
+
+export const AuthControllers = {
+    login
+}
---
+++ src/app/modules/auth/auth.service.ts
@@ -0,0 +1,45 @@
+import httpStatusCode from "http-status-codes";
+import AppError from "../../errorHelpers/appError";
+import { IUser } from "../user/user.interface";
+import { User } from "../user/user.model";
+import { comparePassword } from "../../utils/password";
+
+const login = async (payload: Partial<IUser>) => {
+  const { email, password: pass } = payload;
+
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw new AppError(
+      httpStatusCode.BAD_REQUEST,
+      "Invalid email or password.",
+    );
+  }
+
+  if (user.isActive !== "ACTIVE") {
+    throw new AppError(httpStatusCode.FORBIDDEN, "Your account is not active.");
+  }
+
+//   if (!user.isVerified) {
+//     throw new AppError(
+//       httpStatusCode.FORBIDDEN,
+//       "You are not verified. Please verify your account",
+//     );
+//   }
+
+  const isPassMatch = await comparePassword(pass as string, user.password);
+
+  if (!isPassMatch) {
+    throw new AppError(httpStatusCode.BAD_REQUEST, "Incorrect Password");
+  }
+
+  const userObject = user.toObject();
+
+  const { password, ...userWithoutPassword } = userObject;
+
+  return userWithoutPassword;
+};
+
+export const AuthServices = {
+  login,
+};
