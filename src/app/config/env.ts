b9548e27b91b8f41fd72089cc06a76a23b10126e
rifat-mahmudul <rifatmahmudul.dev@gmail.com>
2026-09-16 10:28:36 +0600

    feat: implement JWT authentication by adding token generation and user token creation utilities

--- src/app/config/env.ts
+++ src/app/config/env.ts
@@ -7,10 +7,23 @@ interface EnvConfig {
   DB_URL: string;
   NODE_ENV: "development" | "production";
   SALT_ROUNDS: number;
+  JWT_ACCESS_SECRET: string;
+  JWT_REFRESH_SECRET: string;
+  JWT_ACCESS_EXPIRES: string;
+  JWT_REFRESH_EXPIRES: string;
 }
 
 const loadEnvVariables = (): EnvConfig => {
-  const requiredEnvVariables = ["PORT", "DB_URL", "NODE_ENV", "SALT_ROUNDS"];
+  const requiredEnvVariables = [
+    "PORT",
+    "DB_URL",
+    "NODE_ENV",
+    "SALT_ROUNDS",
+    "JWT_ACCESS_SECRET",
+    "JWT_REFRESH_SECRET",
+    "JWT_ACCESS_EXPIRES",
+    "JWT_REFRESH_EXPIRES",
+  ];
 
   requiredEnvVariables.forEach((key) => {
     if (!process.env[key]) {
@@ -23,6 +36,10 @@ const loadEnvVariables = (): EnvConfig => {
     DB_URL: process.env.DB_URL!,
     NODE_ENV: process.env.NODE_ENV as "development" | "production",
     SALT_ROUNDS: Number(process.env.SALT_ROUNDS),
+    JWT_ACCESS_SECRET: process.env.JWT_ACCESS_SECRET as string,
+    JWT_REFRESH_SECRET: process.env.JWT_REFRESH_SECRET as string,
+    JWT_ACCESS_EXPIRES: process.env.JWT_ACCESS_EXPIRES as string,
+    JWT_REFRESH_EXPIRES: process.env.JWT_REFRESH_EXPIRES as string,
   };
 };
 
--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -3,6 +3,7 @@ import AppError from "../../errorHelpers/appError";
 import { IUser } from "../user/user.interface";
 import { User } from "../user/user.model";
 import { comparePassword } from "../../utils/password";
+import { createUserTokens } from "../../utils/userTokens";
 
 const login = async (payload: Partial<IUser>) => {
   const { email, password: pass } = payload;
@@ -17,15 +18,18 @@ const login = async (payload: Partial<IUser>) => {
   }
 
   if (user.isActive !== "ACTIVE") {
-    throw new AppError(httpStatusCode.FORBIDDEN, "Your account is not active.");
+    throw new AppError(
+      httpStatusCode.UNAUTHORIZED,
+      "Your account is not active.",
+    );
   }
 
-//   if (!user.isVerified) {
-//     throw new AppError(
-//       httpStatusCode.FORBIDDEN,
-//       "You are not verified. Please verify your account",
-//     );
-//   }
+  //   if (!user.isVerified) {
+  //     throw new AppError(
+  //       httpStatusCode.FORBIDDEN,
+  //       "You are not verified. Please verify your account",
+  //     );
+  //   }
 
   const isPassMatch = await comparePassword(pass as string, user.password);
 
@@ -33,11 +37,17 @@ const login = async (payload: Partial<IUser>) => {
     throw new AppError(httpStatusCode.BAD_REQUEST, "Incorrect Password");
   }
 
+  const userTokens = createUserTokens(user);
+
   const userObject = user.toObject();
 
   const { password, ...userWithoutPassword } = userObject;
 
-  return userWithoutPassword;
+  return {
+    accessToken: userTokens.accessToken,
+    refreshToken: userTokens.refreshToken,
+    user: userWithoutPassword,
+  };
 };
 
 export const AuthServices = {
