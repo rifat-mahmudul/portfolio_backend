3618d58e159256f6a7dca830aa08d2b4e0bd9de4
rifat-mahmudul <rifatmahmudul.dev@gmail.com>
2026-09-16 10:37:47 +0600

    refactor: improve formatting and consistency in JWT utility functions

--- src/app/utils/jwt.ts
+++ src/app/utils/jwt.ts
@@ -1,10 +1,17 @@
 import jwt, { JwtPayload, SignOptions } from "jsonwebtoken";
-import { envVars } from "../config/env";
 
-export const generateToken = (payload: JwtPayload, secret: string, expiresIn: string) => {
-    const token = jwt.sign(payload, secret, {
-        expiresIn
-    } as SignOptions)
+export const generateToken = (
+  payload: JwtPayload,
+  secret: string,
+  expiresIn: string,
+) => {
+  const token = jwt.sign(payload, secret, {
+    expiresIn,
+  } as SignOptions);
 
-    return token
-}
+  return token;
+};
+
+export const verifyToken = (token: string, secret: string) => {
+  return jwt.verify(token, secret);
+};
