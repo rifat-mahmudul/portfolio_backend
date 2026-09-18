672edc0460be9f5ce68e487f9b603af0b8eaa59f
rifat-mahmudul <rifatmahmudul.dev@gmail.com>
2026-09-18 11:07:36 +0600

    feat: add email sending functionality with nodemailer and template support

--- src/app/config/env.ts
+++ src/app/config/env.ts
@@ -11,6 +11,13 @@ interface EnvConfig {
   JWT_REFRESH_SECRET: string;
   JWT_ACCESS_EXPIRES: string;
   JWT_REFRESH_EXPIRES: string;
+  EMAIL_SENDER: {
+    SMTP_USER: string;
+    SMTP_PASS: string;
+    SMTP_PORT: string;
+    SMTP_HOST: string;
+    SMTP_FROM: string;
+  };
 }
 
 const loadEnvVariables = (): EnvConfig => {
@@ -23,6 +30,11 @@ const loadEnvVariables = (): EnvConfig => {
     "JWT_REFRESH_SECRET",
     "JWT_ACCESS_EXPIRES",
     "JWT_REFRESH_EXPIRES",
+    "SMTP_PASS",
+    "SMTP_PORT",
+    "SMTP_HOST",
+    "SMTP_USER",
+    "SMTP_FROM",
   ];
 
   requiredEnvVariables.forEach((key) => {
@@ -40,6 +52,13 @@ const loadEnvVariables = (): EnvConfig => {
     JWT_REFRESH_SECRET: process.env.JWT_REFRESH_SECRET as string,
     JWT_ACCESS_EXPIRES: process.env.JWT_ACCESS_EXPIRES as string,
     JWT_REFRESH_EXPIRES: process.env.JWT_REFRESH_EXPIRES as string,
+    EMAIL_SENDER: {
+      SMTP_USER: process.env.SMTP_USER as string,
+      SMTP_PASS: process.env.SMTP_PASS as string,
+      SMTP_PORT: process.env.SMTP_PORT as string,
+      SMTP_HOST: process.env.SMTP_HOST as string,
+      SMTP_FROM: process.env.SMTP_FROM as string,
+    },
   };
 };
 
