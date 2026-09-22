976380555de8b83eaab72a3dd6bc103963af43f1
rifat-mahmudul <rifatmahmudul.dev@gmail.com>
2026-09-22 23:07:36 +0600

    feat: add experience route to main router

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -3,6 +3,7 @@ import { UserRoutes } from "../modules/user/user.route";
 import { AuthRoutes } from "../modules/auth/auth.route";
 import { ProfileRoutes } from "../modules/profile/profile.route";
 import { ProjectRoutes } from "../modules/project/project.routes";
+import { ExperienceRoutes } from "../modules/experience/experience.route";
 
 export const router = Router();
 
@@ -22,6 +23,10 @@ const moduleRoutes = [
   {
     path: "/project",
     route: ProjectRoutes,
+  },
+  {
+    path: "/experience",
+    route: ExperienceRoutes
   }
 ];
 
